e472423b04bde25983c99ce1991279b434f0c728
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2021-10-06 03:30:33 +0900

    main, signup XML 구현 및 인탠트 연결
    main, signup XML 구현 및 인탠트 연결

--- app/src/main/java/com/example/midterm/MainActivity.kt
+++ app/src/main/java/com/example/midterm/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.midterm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        val SignupIntent = Intent(this,SignupActivity::class.java)
+        val btn_signup : Button = findViewById(R.id.btn_signup)
+
+        btn_signup.setOnClickListener{
+            startActivity(SignupIntent)
+        }
+
+
     }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/midterm/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.example.midterm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+    }
+}
